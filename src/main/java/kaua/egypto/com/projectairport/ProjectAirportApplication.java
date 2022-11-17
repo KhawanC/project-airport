0435142d2e3e72de2135f18c93ce5791c8930c2a
KhawanC <kauacassiano121@gmail.com>
2022-11-17 11:35:07 -0300

    starter project

---
+++ src/main/java/kaua/egypto/com/projectairport/ProjectAirportApplication.java
@@ -0,0 +1,13 @@
+package kaua.egypto.com.projectairport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAirportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaua/egypto/com/projectairport/ProjectAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package kaua.egypto.com.projectairport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
